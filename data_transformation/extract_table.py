55eecf64e5c36ee44da57a4774b8a8556a05cf25
Neto-Pereira25 <devneto203@gmail.com>
2025-03-25 23:34:20 -0300

    refactor: ajustes na extração da tabela

--- data_transformation/extract_table.py
+++ data_transformation/extract_table.py
@@ -1,30 +1,28 @@
 import os
-import pdfplumber
+import tabula
 import pandas as pd
 import zipfile
 
-DOWNLOAD_FOLDER = 'data_files'
-PDF_FILE = os.path.join(DOWNLOAD_FOLDER, 'Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf')
-CSV_FILE = 'data_file/rol_de_procedimentos.csv'
-ZIP_FILE = 'data_file/Test_Jose_Pereira_da_Silva_Neto.zip'
+DOWNLOAD_FOLDER = "data_files"
+PDF_FILE = os.path.join(DOWNLOAD_FOLDER, "Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf")  # Nome do arquivo pode variar
+CSV_FILE = "data_files/rol_de_procedimentos.csv"
+ZIP_FILE = "data_files/Teste_JOSE_PEREIRA_DA_SILVA_NETO.zip"
 
 SUBSTITUTIONS = {
-    'OD': 'Seg. Odontológica',
-    'AMB': 'Seg. Ambulatorial',
+    "OD": "Seg. Odontológica",
+    "AMB": "Seg. Ambulatorial"
 }
 
-def extract_table(pdf_path: str) -> list:
-    """ Extrai a tabela do pdf e retorna como um DataFrame do Pandas """
-    data_table = []
+def extract_table(pdf_path):
+    """ Extrai a tabela do PDF e retorna como um DataFrame do Pandas. """
 
-    with pdfplumber.open(pdf_path) as pdf:
-        for page in pdf.pages:
-            tables = page.extract_table()
-            if tables:
-                for line in tables:
-                    data_table.append(line)
+    try:
+        table_list = tabula.read_pdf(pdf_path, pages='3', multiple_tables=True)
+
+        df = pd.concat(table_list, sort=False, ignore_index=True)
+        return df
+    except Exception as e:
+        print(e)
+
+
     
-    return data_table
-
-if __name__ == '__main__':
-    print(extract_table(PDF_FILE))
